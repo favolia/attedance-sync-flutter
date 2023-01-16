90dad757554a924d26ffef5fb9dc6214ad3fb315
arhamcode <arham.code@gmail.com>
2023-01-16 10:52:50 +0800

    Add worklocation entity

---
+++ lib/features/machine_finger/domain/worklocation.dart
@@ -0,0 +1,13 @@
+class WorkLocation {
+  final String name;
+  final String code;
+  final String companyName;
+  final String companyId;
+
+  WorkLocation({
+    required this.name,
+    required this.code,
+    required this.companyName,
+    required this.companyId,
+  });
+}
