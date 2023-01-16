0b37b2cb199b5d0d20630a2dad1ba23f5dd1a238
arhamcode <arham.code@gmail.com>
2023-01-16 10:53:10 +0800

    Add machine_finger entity

---
+++ lib/features/machine_finger/domain/machine_finger.dart
@@ -0,0 +1,15 @@
+import 'worklocation.dart';
+
+class MachineFinger {
+  final String ipAddress;
+  final String description;
+  final bool enableDownload;
+  final List<WorkLocation> worklocations;
+
+  MachineFinger({
+    required this.ipAddress,
+    required this.description,
+    required this.enableDownload,
+    required this.worklocations,
+  });
+}
